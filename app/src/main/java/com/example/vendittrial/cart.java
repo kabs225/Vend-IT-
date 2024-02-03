cd9ba574443ce2994cc1b46f35010a72c641e20e
Kabeer Shivani <kabs225@hotmail.com>
2024-02-03 16:00:29 +0530

    cart page

--- app/src/main/java/com/example/vendittrial/cart.java
+++ app/src/main/java/com/example/vendittrial/cart.java
@@ -2,13 +2,31 @@ package com.example.vendittrial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class cart extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        button = (Button) findViewById(R.id.btn_backcart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                itemSelectionPage();
+            }
+        });
+
+    }
+    public void itemSelectionPage() {
+        Intent intent = new Intent(this, itemSelectionPage.class);
+        startActivity(intent);
     }
 }
