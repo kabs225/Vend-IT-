ceb82cdf578d3be5e43708c9e4d00b8aaa852ce7
Kabeer Shivani <kabs225@hotmail.com>
2024-03-09 14:01:06 +0530

    kabeer 9-03

--- app/src/main/java/com/example/vendittrial/profile_page.java
+++ app/src/main/java/com/example/vendittrial/profile_page.java
@@ -1,4 +1,25 @@
 package com.example.vendittrial;
 
-public class profile_page {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class profile_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+
+    }
+    public void openMachineSelectionPage()
+    {
+        Intent intent = new Intent(this, MachineSelectionPage.class);
+        startActivity(intent);
+    }
 }
